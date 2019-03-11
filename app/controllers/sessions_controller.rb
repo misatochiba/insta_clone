8d94f1142f89fff7e3a920be2648922746906a44
misatochiba <new.mi.start@gmail.com>
2019-03-11 12:32:53 +0000

    change view about login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash.now[:danger] = 'パスワードが間違っています。パスワードをご確認ください。' # 本当は正しくない
+      render 'new'
+    end
   end
 
   def destroy
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
 
-  root 'users#new'
+  root 'sessions#new'
+  get  '/users/new',    to: 'users#new'
   get  '/home',    to: 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
