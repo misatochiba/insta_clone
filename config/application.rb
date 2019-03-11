4edadf28c3bab959e5567b681e5ffbdcc2bcf0d9
misatochiba <new.mi.start@gmail.com>
2019-03-11 11:07:41 +0000

    for heroku changed config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,4 +15,6 @@ module InstaClone
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+  
+  config.assets.initialize_on_precompile = false
 end
