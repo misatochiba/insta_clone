57793a8ffb8af49290c5b3e76329753042de21c1
misatochiba <new.mi.start@gmail.com>
2019-03-12 15:13:20 +0000

    picture update validates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,15 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
