834767f33f8ce4ab6f85a4dd832df67f030a9818
misatochiba <new.mi.start@gmail.com>
2019-03-13 16:14:05 +0000

    account activated by test email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,9 +11,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      remember user
-      redirect_to user
+      if user.activated?
+        log_in user
+        remember user
+        redirect_to user
+      else
+        message  = "アカウントが有効ではありません"
+        message += "メールのリンクからログインしてください"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,16 +22,17 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+    # トークンがダイジェストと一致したらtrueを返す
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+  
   # 永続セッションのためにユーザーをデータベースに記憶する
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
-  end
-
-  # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   # ユーザーのログイン情報を破棄する
