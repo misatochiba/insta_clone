0a8175736fe46c9a58f0830d04a21ce21c2fb5e4
misatochiba <new.mi.start@gmail.com>
2019-03-21 06:55:54 +0000

    move follow button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
-
+  before_action :logged_in_user, only: [:index,:destroy,
+                                        :following, :followers]
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -40,6 +41,20 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+    def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,1 @@
-require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get root_path
-    assert_response :success
-  end
-
-end
