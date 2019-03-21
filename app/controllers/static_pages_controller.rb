8667e818f147f14ec7dd4ebe2e0b55719b20c8eb
misatochiba <new.mi.start@gmail.com>
2019-03-21 11:52:50 +0000

    change profile view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    @user = current_user
     @micropost  = current_user.microposts.build
+    @microposts = current_user.microposts.paginate(page: params[:page])
     @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
