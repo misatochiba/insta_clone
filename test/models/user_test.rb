413eed81030a9ce4187fb107bb13f1bfca960971
misatochiba <new.mi.start@gmail.com>
2019-03-21 06:20:18 +0000

    follow validates

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,5 +6,15 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
+  
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
 
 end
