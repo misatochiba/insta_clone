4582c56909fa9167e75d19e85273ba01da995265
misatochiba <new.mi.start@gmail.com>
2019-03-21 12:01:43 +0000

    change view layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,6 +10,10 @@ class StaticPagesController < ApplicationController
   end
 
   def about
+    @user = current_user
+    @micropost  = current_user.microposts.build
+    @microposts = current_user.microposts.paginate(page: params[:page])
+    @feed_items = current_user.feed.paginate(page: params[:page])
   end
   
   def contact
