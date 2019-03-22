f3db3e5130a0189643a9d14e09ae9628695cd463
misatochiba <new.mi.start@gmail.com>
2019-03-22 11:16:27 +0000

    sucsess all test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,33 +16,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
   
-  test "login with valid information" do
-    get root_path
-    post login_path, params: { session: { email:    @user.email,
-                                          password: 'password' } }
-    assert is_logged_in?
-    assert_redirected_to @user
-    assert_select "a[href=?]", root_path, count: 0
-    delete logout_path
-    assert_not is_logged_in?
-    assert_redirected_to root_url
-    assert_select "a[href=?]", user_path(@user), count: 0
-  end
-  
-  test "login with valid information followed by logout" do
-    get login_path
-    post login_path, params: { session: { email:    @user.email,
-                                          password: 'password' } }
-    assert is_logged_in?
-    assert_redirected_to @user
-    delete logout_path
-    assert_not is_logged_in?
-    assert_redirected_to root_url
-    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
-    delete logout_path
-    follow_redirect!
-    assert_select "a[href=?]", logout_path,      count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
-  end
+
   
 end
