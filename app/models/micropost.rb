38216f3c9f7de81e40c5e8c72ae4ba3c4fcbbd2f
misatochiba <new.mi.start@gmail.com>
2019-03-25 10:39:44 +0000

    Add user microposts for production

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,4 +5,14 @@ class Micropost < ApplicationRecord
   validates :picture, presence: true
   validates :user_id, presence: true
   validates :content, length: { maximum: 140 }
+  validate  :picture_size
+  
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
